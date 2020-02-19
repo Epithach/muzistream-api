875d4b2cbfce286da4dcfe0be9b16bc8deecdcf3
Antoine THACH <antoine.thach@gmail.com>
2020-02-19 21:55:14 +0100

    Initial commit : Adding MizistreamApi solution

---
+++ Muzistream-api/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Muzistream_api.Controllers
+{
+    [Route("api/User")]
+    public class UserController : Controller
+    {
+        [HttpGet("{id}")]
+        public void Get(string userReference)
+        {
+        }
+
+        [HttpGet("current")]
+        public void GetCurrentUser()
+        {
+
+        }
+    }
+}
---
+++ Muzistream-api/Services/Oath2Service.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muzistream_api.Services
+{
+    public class Oath2Service
+    {
+    }
+}
